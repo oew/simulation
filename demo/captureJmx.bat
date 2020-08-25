352be085762c58038135eb6a5ddb816198c4af45
oew <everett.williams@yahoo.com>
2020-08-25 11:48:05 -0700

    Update captureJmx.bat

--- demo/captureJmx.bat
+++ demo/captureJmx.bat
@@ -1,2 +1,2 @@
 mkdir tomcat
-java -cp simulation-full.jar -Dlog4j.configurationFile=./log4j2.xml com.ew.capture.jmx.JmxSnapshot localhost 9097 "Catalina" - - "./tomcat/"
+java -cp simulation-full.jar -Dlog4j.configurationFile=./log4j2.xml com.tmm.capture.jmx.JmxSnapshot localhost 9097 "Catalina" - - "./tomcat/"
